ae20fb4b598bcb506a38acf3e2b7862d42f00633
Julian Ronda <jronda26@gmail.com>
2018-03-13 21:13:10 -0400

    Add files via upload

---
+++ The Adoption Site/index.php
@@ -0,0 +1,357 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>The Adoption Shop</title>
+
+<!--
+Newline Template
+http://www.templatemo.com/tm-503-newline
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <link rel="stylesheet" href="css/contact.css">
+
+
+        <link rel="shortcut icon" href="img/page-icon.png"/>
+
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script type="text/javascript" src="js/learnTable.js"></script>
+        <script type="text/javascript" src="js/checkForm.js"></script>
+        <link rel="stylesheet" href="css/learnTable.css">
+    </head>
+    <body>
+
+        <div class="overlay"></div>
+        <section class="top-part">
+          <img src="img/sad-dog.jpeg" />
+
+        </section>
+
+        <section class="cd-hero">
+
+          <div class="cd-slider-nav">
+            <nav>
+              <span class="cd-marker item-1"></span>
+              <ul>
+                <li class="selected"><a href="#0"><div class="image-icon"><img src="img/home-icon.png"></div><h6>Home</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/learn-icon.png" width="60" height="64"></div><h6>Learn</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/dog-icon.png" width="60" height="64" ></div><h6>Adoption</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/projects-icon.png"></div><h6>Login</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/contact-icon.png"></div><h6>Contact Us</h6></a></li>
+              </ul>
+            </nav>
+          </div> <!-- .cd-slider-nav -->
+
+          <ul class="cd-hero-slider">
+
+            <li class="selected">
+              <div class="heading">
+                <h1>The Adoption Site</h1>
+                <span>The one stop shop to learn and adopt A furry friend of your own</span>
+
+              </div>
+              <div class="cd-full-width first-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content first-content">
+                        <h4>A little about us.</h4>
+                        <p> Our mission is to portect and help those four legged friends that need our help. Approximately 6.5 million lonely animals enter U.S. animal shelters nationwide every year. Over half of those our our canine companions. Here at the adoption shop we are trying to stem this issue and using the power of the internet to help. On our site you will be able to find all the information needed to make the best desion possible before you actually adopt a new friend. Take a look through our page, learn about the different breeds and about how thier personalities can work with you! Although we are primarly based in Virginia, let us know where your from and we will try our hardest to help. </p>
+                        <div class="primary-button">
+                          <a href="#">Back To Top</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Looking To Learn about some breeds?</h1>
+                <span>Well look no further!</span>
+              </div>
+              <div class="cd-half-width second-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content second-content">
+                        <div class="row">
+
+                          <h1>Dog Breeds</h1>
+
+                          <input type="text" id="Input" onkeyup="tableSearch()" placeholder="Search for a breed..." title="Search for any breed">
+
+                          <table id="learnTable">
+                            <tr class="header">
+                              <th style="width:65%;">Breed Name</th>
+                              <th style="width:35%;">Learn More</th>
+                            </tr>
+                            <tr>
+                              <td>Beagle</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Beagle"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Bulldog</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Bulldog"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Boxer</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Boxer_(dog)"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Dachshund</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Dachshund"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Doberman Pinscher</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Doberman_Pinscher"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>French Bulldog</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/French_Bulldog"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Rottweiler</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Rottweiler"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Godlen Retriver</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Golden_Retriever"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>German Shepard</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/German_Shepherd"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>German Shorthair Pointer</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/German_Shorthair_Pointer"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Great Danes</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Great_Danes"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Labrador Retriver</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Labrador_Retriever"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Siberian Husky</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Siberian_Husky"target="_blank">Link</a> </td>
+                            </tr>
+                            <tr>
+                              <td>Poodle</td>
+                              <td> <a href="https://en.wikipedia.org/wiki/Poodle"target="_blank">Link</a> </td>
+                            </tr>
+
+                          </table>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+
+<!--###########################################################################################################-->
+
+            <li>
+              <div class="heading">
+                <h1>Featured</h1>
+                <span>Sorry this page is still under construction</span>
+              </div>
+              <div class="cd-half-width third-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content third-content">
+                        <div class="row">
+                          <div class="col-md-7 left-image">
+                            <img src="img/left-feature-image.png">
+                          </div>
+                          <div class="col-md-5">
+                            <div class="right-feature-text">
+                              <h4>Featured Product: <em>Newline</em></h4>
+                              <p>Lorem ipsum dolor amet, consecte adipiscing elit, quisque dictum convallis mi. In dapibus auctor dictum donec mattis quis eros ultricies feugiat. Morbi congue faucibus mi, ague blandit curabitur ac lacinia.</p>
+                              <div class="feature-list">
+                                <ul>
+                                  <p>- Suspendisse mattis finibus sem</p>
+                                  <p>- Pellentesque et urna vel lectus</p>
+                                  <p>- Vestibulum iaculis nisi dui</p>
+                                  <p>- Donec sagittis eros , ac tempus ligula</p>
+                                  <p>- Integer sapien risus, auctor</p>
+                                </ul>
+                              </div>
+                              <div class="primary-button">
+                                <a href="#">Discover More</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Log in</h1>
+                <span>for us to remeber you in the future</span>
+              </div>
+              <div class="cd-half-width fourth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fourth-content">
+                        <div class="row">
+                          <form action="php/login.php" method=post>
+                          <h4> Enter Your Email </h4>
+                           <input type="text" id="emailfield" name="email" placeholder="Your email..">
+                           <br>
+                           <h4> Type in your password </h4>
+                           <input type="password" id="passwordfield" name="password" placeholder="Your password..">
+                           <br>
+                           <input type="submit" description="Click to Log in">
+                          </form>
+                           <br>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Contact us</h1>
+                <span>You'll be responded within 48 hrs</span>
+              </div>
+              <div class="cd-half-width fivth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fivth-content">
+                        <div class="row">
+                          <div class="col-md-5">
+                            <div class="left-info">
+                              <p> Have a question you think you we can answer? Let us know! Just fill out this from with your name, email and inlcude any comments or concerns that you may have any we will do our best to get you a repsonse in the shortest time possible.
+                              <br><br>
+                              <em>301 W. Bark St<br>Pawington CT, 06830 </em>
+                              </p>
+                              <ul class="social-icons">
+                                <i><a href="#"><i class="fa fa-facebook"></i></a></i>
+                                <i><a href="#"><i class="fa fa-twitter"></i></a></i>
+                                <i><a href="#"><i class="fa fa-linkedin"></i></a></i>
+                                <i><a href="#"><i class="fa fa-rss"></i></a></i>
+                                <i><a href="#"><i class="fa fa-behance"></i></a></i>
+                              </ul>
+                              <br>
+                            </div>
+                          </div>
+                          <div class="col-md-7">
+                            <div class="row">
+                              <form name="contactForm" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+
+                                <!-- onsubmit="return checkForm(this);" -->
+                                  <h4> Your first and last name </h4>
+                                   <input type="text" id="inputfield" name="name" placeholder="Your name..">
+                                   <br>
+                                  <h4> Your email address </h4>
+                                   <input type="text" id="emailfield" name="emailaddress" placeholder="Your email address..">
+                                   <br>
+                                   <h2> Your message to us.. </h2>
+                                   <input type="text" id="messagebox" name="message">
+                                   <br>
+                                   <div style="width:100%; text-align:center;">
+                                     <input type="submit" description="Submit">
+                                   </div>
+                            </form>
+
+
+                            <?php
+
+                             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                              $name = $_POST['name'];
+                              $message = $_POST['message'];
+
+
+                             $names = array();
+                             $i = 0;
+
+                             $present = 0;
+
+                             
+                              for ( $i; $i < count($names); $i++){
+
+                                if ($name == $names[i]) {
+                                  $present = 1;
+                                  $names[$name] += "\r\n" + $message;
+                                }
+                              }
+
+                              if  ($present == 0) {
+                                $names[$name] = $message;
+
+                              }
+
+                              $msg = "Thanks " . $name . " your message has been sent!";
+                              echo "<script type='text/javascript'>alert('$msg');</script>";
+
+                              //echo "Thanks " . $name . "your message has been sent!";
+                            }
+
+
+
+
+                             ?>
+
+
+
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul> <!-- .cd-hero-slider -->
+        </section> <!-- .cd-hero -->
+
+
+        <footer>
+          <p>Copyright &copy; 2018 The Adoption Company
+
+        	| Designed by <a href="http://www.templatemo.com" target="_parent"><em>templatemo</em></a></p>
+        </footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/learnTable.js"></script>
+
+    </body>
+</html>
